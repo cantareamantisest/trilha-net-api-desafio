7e141051d179d3abe095581db47c045182b81e32
Karl Anderson <cantareamantisest@gmail.com>
2023-10-28 15:54:30 -0300

    challenge solved

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TrilhaApiDesafio.Context;
 using TrilhaApiDesafio.Models;
 
@@ -16,19 +18,40 @@ namespace TrilhaApiDesafio.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult ObterPorId(int id)
+        public async Task<IActionResult> ObterPorId(int id)
         {
             // TODO: Buscar o Id no banco utilizando o EF
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            try
+            {
+                var tarefa =  await _context.Tarefas.FindAsync(id);
+                if (tarefa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(tarefa);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+           
         }
 
         [HttpGet("ObterTodos")]
-        public IActionResult ObterTodos()
+        public async Task<IActionResult> ObterTodos()
         {
             // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            try
+            {
+                var tarefas = await _context.Tarefas.ToListAsync();
+                return Ok(tarefas);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpGet("ObterPorTitulo")]
@@ -36,14 +59,33 @@ namespace TrilhaApiDesafio.Controllers
         {
             // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
             // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
+            try
+            {
+                var tarefa = _context.Tarefas.Where(c => c.Titulo == titulo);
+                return Ok(tarefa);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpGet("ObterPorData")]
-        public IActionResult ObterPorData(DateTime data)
+        public IActionResult ObterPorData(string data)
         {
-            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
-            return Ok(tarefa);
+            try 
+            {
+                if (DateTime.TryParse(data, out DateTime dataParse))
+                {
+                    var tarefa = _context.Tarefas.Where(x => x.Data.Date == dataParse.Date);
+                    return Ok(tarefa);
+                }
+                return BadRequest("O formato da data está errado!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }            
         }
 
         [HttpGet("ObterPorStatus")]
@@ -51,46 +93,85 @@ namespace TrilhaApiDesafio.Controllers
         {
             // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
             // Dica: Usar como exemplo o endpoint ObterPorData
-            var tarefa = _context.Tarefas.Where(x => x.Status == status);
-            return Ok(tarefa);
+            try 
+            {
+                var tarefa = _context.Tarefas.Where(x => x.Status == status);
+                return Ok(tarefa);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }              
         }
 
         [HttpPost]
-        public IActionResult Criar(Tarefa tarefa)
+        public async Task<IActionResult> Criar(Tarefa tarefa)
         {
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
-            return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+            try 
+            {
+                if (ModelState.IsValid)
+                {
+                    await _context.Tarefas.AddAsync(tarefa);
+                    await _context.SaveChangesAsync();
+                    return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+                }
+                return BadRequest();   
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }            
         }
 
         [HttpPut("{id}")]
-        public IActionResult Atualizar(int id, Tarefa tarefa)
+        public async Task<IActionResult> Atualizar(int id, Tarefa tarefa)
         {
-            var tarefaBanco = _context.Tarefas.Find(id);
+            try 
+            {
+                if (ModelState.IsValid)
+                {
+                    var tarefaBanco = await _context.Tarefas.FindAsync(id);
 
-            if (tarefaBanco == null)
-                return NotFound();
+                    if (tarefaBanco == null)
+                        return NotFound();
 
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+                    // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
+                    // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+                    tarefaBanco.Titulo = tarefa.Titulo;
+                    tarefaBanco.Descricao = tarefa.Descricao;
+                    tarefaBanco.Data = tarefa.Data;
+                    tarefaBanco.Status = tarefa.Status;
+                    _context.Tarefas.Update(tarefaBanco);
+                    await _context.SaveChangesAsync();
+                    return Ok("Tarefa atualizado com sucesso!");
+                }   
+                return BadRequest();             
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }            
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Deletar(int id)
+        public async Task<IActionResult> Deletar(int id)
         {
-            var tarefaBanco = _context.Tarefas.Find(id);
+            try 
+            {
+                var tarefaBanco = await _context.Tarefas.FindAsync(id);
 
-            if (tarefaBanco == null)
-                return NotFound();
+                if (tarefaBanco == null)
+                    return NotFound();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
-            return NoContent();
+                // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+                _context.Tarefas.Remove(tarefaBanco);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }            
         }
     }
 }
--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,16 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+        [Key]
+        [JsonIgnore]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Titulo { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Descricao { get; set; }
-        public DateTime Data { get; set; }
-        public EnumStatusTarefa Status { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; } = DateTime.Now;
+
+        [Required]
+        public EnumStatusTarefa Status { get; set; } = EnumStatusTarefa.Pendente;
     }
 }
